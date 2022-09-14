162ba70a408f7ef209c107bc52821b111c3e068c
Hathaway-Shan <hathaway.shan@gmail.com>
2022-09-14 12:53:41 -0700

    subtitle text is displaying properly and changes with event

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,12 +8,13 @@ import './Home.css';
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
   const [title, setTitle] = useState('');
+  const [subTitle, setSubTitle] = useState('');
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} />
-      <Editor title={title} setTitle={setTitle} />
+      <Preview title={title} subTitle={subTitle} />
+      <Editor title={title} setTitle={setTitle} subTitle={subTitle} setSubTitle={setSubTitle} />
     </main>
   );
 }
--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview({ title }) {
+export default function Preview({ title, subTitle }) {
   // implement a Preview screen here
   //  Note - the HTML should have the following structure
   //  the main div should have a class of preview and the font-name prop
@@ -11,7 +11,7 @@ export default function Preview({ title }) {
   return (
     <div className="preview">
       <h1>{title}</h1>
-      <h3> Blog Subtitle</h3>
+      <h3>{subTitle}</h3>
       <p>This is some blog text. </p>
     </div>
   );
